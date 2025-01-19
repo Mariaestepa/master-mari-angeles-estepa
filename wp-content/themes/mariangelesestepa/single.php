8ac11a9ccb324c9a35f4783f441e217531358aa2
Mariaestepa <Maestepamadera@hotmail.com>
2025-01-19 15:12:11 +0100

    Categorías e instalación de plugin campos personalizados

--- wp-content/themes/mariangelesestepa/single.php
+++ wp-content/themes/mariangelesestepa/single.php
@@ -1,14 +1,16 @@
 <?php
 get_header();
 ?>
-
-<h1>
-     <?php the_title();?>
-</h1>
-<?php
-echo the_content();
-?>
-
+<div class="generico">
+     <h1>
+          <?php the_title();?>
+     </h1>
+ <section id="contenido">
+     <?php
+     echo the_content();
+     ?>
+</section>
+</div>
 <?php
 get_footer();
 ?>
