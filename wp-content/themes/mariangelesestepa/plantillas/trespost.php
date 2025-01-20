3d2df440099a4281ace384611ebcec34ccf5b6b0
Carlos Sánchez Donate <43948976+barvock@users.noreply.github.com>
2025-01-20 08:03:15 +0100

    Solo fallaban espacios en blanco extraños
    Puede deberse a copiar o pegar. Si quieres echar las frases a la derecha, mejor usar tabulador.
    
    El resto del código estaba perfecto

--- wp-content/themes/mariangelesestepa/plantillas/trespost.php
+++ wp-content/themes/mariangelesestepa/plantillas/trespost.php
@@ -1,18 +1,18 @@
 <section id="displaytresposts" class="flexcenter">
  <?php
  $args = array(
-        //-1 es para ver todos los posts//
+       //-1 es para ver todos los posts//
     'posts_per_page' => 3,
     'post_type' => 'post',
     'orderby' => 'date', //poniendolo como rand se ordenan de manera aleatoria//
     'order' => 'DESC',
  ); //quedan todos los posts ordenados por fecha en forma descendente//
  $the_query = new WP_Query( $args);
-    //un loop
+   //un loop
  if ( $the_query->have_posts()){
  while ( $the_query->have_posts() ) {
          $the_query->the_post();
-                //include
+//include
 
 include $plantillas .'/display-posts/card-posts.php';
  }
